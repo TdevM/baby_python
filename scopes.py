868bb0daac94b2032b3dc919d0a9a92d1c9eae9b
tdevm <cr14b17@gmail.com>
2019-08-30 02:51:13 +0530

    global

--- scopes.py
+++ scopes.py
@@ -6,9 +6,17 @@ yoImAGlobalVariable = 'Everybody can see me'
 def hidden_room():
     i_live_in_hidden_room = 'I have a lot of secrets'
     print(i_live_in_hidden_room)
-    print('I can see you' + yoImAGlobalVariable)
-    print(yoImAGlobalVariable)
+    print('I can see you: ' + yoImAGlobalVariable)
     return 'I Refuse to return anything'
 
 
+def another_bunker():
+    global iAmAGlobal
+    iAmAGlobal = 'You are a global from the bunker'
+    return 'boom'
+
+
 hidden_room()
+another_bunker()
+
+print(iAmAGlobal)
