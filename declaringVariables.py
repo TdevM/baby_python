c324b9d5cb35b3bdf286297bbac41ef753064b82
tdevm <cr14b17@gmail.com>
2019-08-30 02:21:34 +0530

    b

--- declaringVariables.py
+++ declaringVariables.py
@@ -1,3 +1,5 @@
-myVariable = 7
+myVariable = 7  # I have just declared a variable here
+print(myVariable)  # I can use uppercase in variable names but functions won't allow me to do that.
+
 
 
--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 
-
+print('Hello World')
+print("I can also say hello world like this")
 
 if 7 + 1 == 0:
     print('Hello world!')
