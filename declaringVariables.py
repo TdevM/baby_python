098578708e86c1a9c3732e7ccc632a6020eb60cb
tdevm <cr14b17@gmail.com>
2019-08-30 02:34:07 +0530

    variables types can be changed after being declared

--- declaringVariables.py
+++ declaringVariables.py
@@ -1,5 +1,17 @@
-myVariable = 7  # I have just declared a variable here
+myVariable = 7  # I have just declared a variable of type int here
 print(myVariable)  # I can use uppercase in variable names but functions won't allow me to do that.
 
+myVariable = 8
+print(myVariable)  # This will now print 8
 
+singleQuotedStringVariable = 'This is a single quoted string variable'  # a string variable
+doubleQuotedStringVariable = "This is a double quoted string variable"
 
+print(singleQuotedStringVariable + doubleQuotedStringVariable)  # Concatenation just like JS
+
+okayIAmInteger = 8
+print(okayIAmInteger)  # I am a happy integer
+print(type(okayIAmInteger))  # <class 'int'>
+okayIAmInteger = 'You will be a string now'
+print(type(okayIAmInteger))  # <class 'str'>
+print(okayIAmInteger)  # Woot I'm a string?
