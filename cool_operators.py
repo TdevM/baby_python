88e505bf7fa36348cdf730fc3ccdf996f0bc5fe1
tdevm <cr14b17@gmail.com>
2019-08-30 03:56:24 +0530

    cool ops

---
+++ cool_operators.py
@@ -0,0 +1,9 @@
+my_variable = 7
+your_variable = 7
+enemy_variable = 89991101
+
+# Python Identity Operators
+
+print(my_variable is your_variable)  # >> True
+print(my_variable is enemy_variable)  # >> False
+print(enemy_variable is not my_variable)  # >> True
