58e16e384cb45e4d7ffeafea71bbd8441b9ff706
tdevm <cr14b17@gmail.com>
2019-08-30 02:19:01 +0530

    a

---
+++ functions.py
@@ -0,0 +1,10 @@
+def my_function():
+    print('This is a function')
+    return 'Yo'
+
+
+print(my_function)    # This will just print the function address
+print(my_function())  # This will print statement inside of the function as well as the result of return
+
+
+
---
+++ hello.py
@@ -0,0 +1,7 @@
+
+
+
+if 7 + 1 == 0:
+    print('Hello world!')
+else:
+    print('Never mind world!')
