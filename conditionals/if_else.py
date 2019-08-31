502be8da0bd9ad4083a6cfe86b16e382c2bc9f19
tdevm <cr14b17@gmail.com>
2019-09-01 03:32:13 +0530

    ifelse

---
+++ conditionals/if_else.py
@@ -0,0 +1,11 @@
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")  # >> b is greater than a
+
+a = 33
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:  # Replacement of else if
+    print("a and b are equal")  # >> a and b are equal
--- functions.py
+++ functions.py
@@ -3,8 +3,14 @@ def my_function():
     return 'Yo'
 
 
-print(my_function)    # This will just print the function address
+print(my_function)  # This will just print the function address
 print(my_function())  # This will print statement inside of the function as well as the result of return
 
 
+def pass_me_some_snacks(chips, candies, cola):
+    thank_you = 'I\'m having {} and {} with {} as my snack today. Thank you for the treat!'
+    return thank_you.format(chips, candies, cola)
 
+
+print(pass_me_some_snacks('Too yumm', 'Mazelo', 'Sprite'))
+# >> I'm having Too yumm and Mazelo with Sprite as my snack today. Thank you for the treat!
